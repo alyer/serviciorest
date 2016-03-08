ddd76eb96dc2f156d51c073927329cc695bc8024
Alexander Yerson Romero Sernaqué <ay.romero@mnemo.com>
2016-03-08 16:18:47 +0100

    se crea el metodo que devuelve todas las personas

--- serviciorest/src/main/java/com/alyers/serviciorest/controllers/PersonController.java
+++ serviciorest/src/main/java/com/alyers/serviciorest/controllers/PersonController.java
@@ -1,8 +1,19 @@
 package com.alyers.serviciorest.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alyers.serviciorest.beans.Persona;
 
 @RestController 
 public class PersonController {
-
+	public List<Persona> getPersona(){
+		List <Persona> lp= new ArrayList<Persona>();
+		for (int i=0; i<10; i++){
+			Persona p = new Persona();
+		}
+		return null;
+	}
 }
