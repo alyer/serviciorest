6f177e122aeff83619e362e42e2a15843011a807
Alexander Yerson Romero Sernaqué <ay.romero@mnemo.com>
2016-03-08 16:56:18 +0100

    se agregan parametros a las clases

--- serviciorest/src/main/java/com/alyers/serviciorest/beans/Pais.java
+++ serviciorest/src/main/java/com/alyers/serviciorest/beans/Pais.java
@@ -1,5 +1,18 @@
 package com.alyers.serviciorest.beans;
 
 public class Pais {
-
+	private String idPais;
+	private String pais;
+	public String getIdPais() {
+		return idPais;
+	}
+	public void setIdPais(String idPais) {
+		this.idPais = idPais;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 }
--- serviciorest/src/main/java/com/alyers/serviciorest/controllers/PaisController.java
+++ serviciorest/src/main/java/com/alyers/serviciorest/controllers/PaisController.java
@@ -1,5 +1,17 @@
 package com.alyers.serviciorest.controllers;
 
-public class PaisController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alyers.serviciorest.beans.Pais;
+
+@RestController
+public class PaisController {
+	
+	@RequestMapping("/allpais")
+	public List<Pais> getAllPais(){
+		return null;
+	}
 }
